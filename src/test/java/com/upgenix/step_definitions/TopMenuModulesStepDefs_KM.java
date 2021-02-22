7aa20ad8da99c73e7961fe22e1c336cebeed53d0
KuMuhammet <kuwwatt@gmail.com>
2021-02-23 00:13:39 +0100

    Some codes in step definition

--- src/test/java/com/upgenix/step_definitions/TopMenuModulesStepDefs_KM.java
+++ src/test/java/com/upgenix/step_definitions/TopMenuModulesStepDefs_KM.java
@@ -1,13 +1,17 @@
 package com.upgenix.step_definitions;
 
+import com.upgenix.pages.DashboardsPage;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
+
+import java.util.List;
 
 public class TopMenuModulesStepDefs_KM {
 
     @Then("User should be able to see all modules on the top menu")
-    public void user_should_be_able_to_see_all_modules_on_the_top_menu(io.cucumber.datatable.DataTable dataTable) {
+    public void user_should_be_able_to_see_all_modules_on_the_top_menu(List<String> expectedModuleNames) {
 
+        Assert.assertEquals(expectedModuleNames, new DashboardsPage().modules());
 
     }
 }
