8eea20bc80f667360219e6dbf75c6fdb45000c71
cemal <cemaltoptas55@gmail.com>
2021-02-23 09:13:13 +0100

    ModuleLinksFunctionalityStepDef_Cemal updated!

--- src/test/java/com/upgenix/step_definitions/ModuleLinksFunctionalityStepDef_Cemal.java
+++ src/test/java/com/upgenix/step_definitions/ModuleLinksFunctionalityStepDef_Cemal.java
@@ -1,6 +1,10 @@
 package com.upgenix.step_definitions;
 
+import com.upgenix.pages.DashboardsPage;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+import java.util.List;
 
 public class ModuleLinksFunctionalityStepDef_Cemal {
 
@@ -8,10 +12,14 @@ public class ModuleLinksFunctionalityStepDef_Cemal {
     @Then("verify the {string}")
     public void verify_the(String expectedUserAccountName) {
 
+        Assert.assertEquals(expectedUserAccountName, new DashboardsPage().getUserName());
     }
 
     @Then("the user clicks on the modules to verify following page titles respectively")
-    public void the_user_clicks_on_the_modules_to_verify_following_page_titles_respectively(io.cucumber.datatable.DataTable dataTable) {
+    public void the_user_clicks_on_the_modules_to_verify_following_page_titles_respectively(List<String> expectedPageTitles) {
+
+        Assert.assertEquals(expectedPageTitles, new DashboardsPage().verifyingAccessByNavigatingModules());
 
     }
 }
+
