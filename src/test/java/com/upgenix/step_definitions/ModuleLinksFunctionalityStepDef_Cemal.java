17768aa41c03f830b547dfac82443e60992bf524
cemal <cemaltoptas55@gmail.com>
2021-02-23 09:05:27 +0100

    Two methods added to ModuleLinksFunctionalityStepDef_Cemal

--- src/test/java/com/upgenix/step_definitions/ModuleLinksFunctionalityStepDef_Cemal.java
+++ src/test/java/com/upgenix/step_definitions/ModuleLinksFunctionalityStepDef_Cemal.java
@@ -1,4 +1,17 @@
 package com.upgenix.step_definitions;
 
+import io.cucumber.java.en.Then;
+
 public class ModuleLinksFunctionalityStepDef_Cemal {
+
+    //This method is used to verify User Account Name after logging successfully in
+    @Then("verify the {string}")
+    public void verify_the(String expectedUserAccountName) {
+
+    }
+
+    @Then("the user clicks on the modules to verify following page titles respectively")
+    public void the_user_clicks_on_the_modules_to_verify_following_page_titles_respectively(io.cucumber.datatable.DataTable dataTable) {
+
+    }
 }
